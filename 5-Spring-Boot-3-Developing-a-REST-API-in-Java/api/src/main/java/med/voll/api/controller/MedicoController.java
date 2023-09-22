8ddc51ede9fb26d631e6f0882416bbef980e808b
DanielRs <cdanielrs21@gmail.com>
2023-09-22 17:37:55 -0600

    Primer curso de java spring boot terminado

---
+++ 5-Spring-Boot-3-Developing-a-REST-API-in-Java/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,54 @@
+package med.voll.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.DatosActualizarMedico;
+import med.voll.api.medico.DatosListadoMedico;
+import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico) {
+        medicoRepository.save(new Medico(datosRegistroMedico));
+    }
+
+    @GetMapping
+    public Page<DatosListadoMedico> listarMedicos(@PageableDefault(size = 10) Pageable pageable) {
+        return medicoRepository.findByActivoTrue(pageable).map(DatosListadoMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosActualizarMedico) {
+        Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
+        medico.actualizarDatos(datosActualizarMedico);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void eliminarMedico(@PathVariable Long id) {
+        Medico medico = medicoRepository.getReferenceById(id);
+        medico.desactivarMedico();
+    }
+}
---
+++ 5-Spring-Boot-3-Developing-a-REST-API-in-Java/api/src/main/java/med/voll/api/medico/DatosListadoMedico.java
@@ -0,0 +1,13 @@
+package med.voll.api.medico;
+
+public record DatosListadoMedico(
+        Long id,
+        String nombre,
+        Especialidad especialidad,
+        String documento,
+        String email) {
+
+    public DatosListadoMedico(Medico medico) {
+        this(medico.getId(), medico.getNombre(), medico.getEspecialidad(), medico.getDocumento(), medico.getEmail());
+    }
+}
